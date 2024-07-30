5b69c4b1603f978c596dcf90b2ef0bfaee605563
Marco <marco.a.romero0303@gmail.com>
2024-07-29 23:51:41 -0700

    fixed dependencies issues. working on navigation bar and home page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import '../styles/navBar.scss';
 
 function NavBar(){
     return(
-        <div className="header-nav">
-            <h2>test</h2>
+        <div className="header-section">
+            <div className="nav-bar">
+                <div className="nav-links">
+                    <ul>
+                        <li>Home</li>
+                        <li>Food Menu</li>
+                        <li>About</li>
+                        <li>Other</li>
+                    </ul>
+                </div>
+                <div className="nav-brand">
+                    <h1>Next Meal</h1>
+                </div>
+                <div className="nav-other-choices">
+                    <ul>
+                        <li>Login</li>
+                        <li>Search</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "../styles/Home.scss";
+
+function Home(){
+    return(
+        <div className="home-content">
+            <section className="intro-section">
+                <div className="intro-txt">
+
+                </div>
+                <div className="search-content">
+                    <div className="search-bar">
+                    
+                    </div>
+                    <div className="search-buttons">
+                    
+                    </div>                    
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Home;
