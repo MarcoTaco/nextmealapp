123fb0b6e9a3c63b07de73d247caf92228751db6
Marco <marco.a.romero0303@gmail.com>
2024-07-31 23:33:28 -0700

    working on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,11 @@ function Home(){
         <div className="home-content">
             <section className="intro-section">
                 <div className="intro-txt">
-
+                    <h3>Let's figure out your next meal!</h3>
                 </div>
                 <div className="search-content">
                     <div className="search-bar">
-                    
+                        <input name="ingredients-list" placeholder="Enter a max of 5 ingredients."/>
                     </div>
                     <div className="search-buttons">
                     
