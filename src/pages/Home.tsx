fac357c64ccdd12cd9a42b00172035367ffdc3b3
Marco <marco.a.romero0303@gmail.com>
2024-08-22 23:37:21 -0700

    working on search ingredients section on home page. mostly done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import "../styles/Home.scss";
 
 function Home(){
+    const [inputText, setInputText] = useState<string>("");
+    const [ingredients, setIngredients] = useState<string[]>([]);
+
+    function handleInput(event: ChangeEvent<HTMLInputElement>){
+        setInputText(event.target.value);
+    }
+
+    function handleAdd(){
+        if(inputText != "" && ingredients.length <= 5){
+            setIngredients([...ingredients, inputText]);
+        }
+        else if(ingredients.length > 5){
+            setInputText("No more ingredients");
+        }
+        setInputText("");
+    }
+
+    function handleClear(){
+        setIngredients([]);
+    }
+    
     return(
         <div className="home-content">
             <section className="intro-section">
@@ -10,10 +31,17 @@ function Home(){
                 </div>
                 <div className="search-content">
                     <div className="search-bar">
-                        <input name="ingredients-list" placeholder="Enter a max of 5 ingredients."/>
+                        <input type="text" value={inputText} onChange={handleInput} placeholder="Max 5 Ingredients"/>
+                        <button onClick={handleAdd}>Add Ingredient</button>
+                        <button>Search</button>
+                        <button onClick={handleClear}>Clear</button> 
                     </div>
-                    <div className="search-buttons">
-                    
+                    <div className="search-display">
+                        <ul>
+                            {ingredients.map((text, index) => (
+                                <li key={index}>{text}</li>
+                            ))}
+                        </ul>
                     </div>                    
                 </div>
             </section>
