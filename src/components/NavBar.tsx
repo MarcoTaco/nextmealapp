70df707f7ac14ee9baf3910ce41ca5565ab3f0c4
Marco <marco.a.romero0303@gmail.com>
2025-01-19 17:06:25 -0800

    mostly done with navigation bar on desktop view just need mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,35 +1,26 @@
 import React from 'react';
 import SearchFoodBtn from './SearchFoodBtn.js';
 import '../styles/navBar.scss';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function NavBar(){
-    const navigate = useNavigate();
-
-    function handleHomeClick(){
-        navigate('/');
-    }
-    
     return(
         <div className="header-section">
             <div className="nav-bar">
+                <div className="nav-brand">
+                    <h1><Link to="/">Next Meal</Link></h1>
+                </div>
+                <div className="nav-other-choices">
+                    <SearchFoodBtn />
+                </div>
                 <div className="nav-links">
                     <ul>
-                        <li onClick={handleHomeClick} style={{cursor: 'pointer'}}>Home</li>
+                        <li><Link to="/">Home</Link></li>
                         <li>Food Menu</li>
                         <li>About</li>
                         <li>Other</li>
-                    </ul>
-                </div>
-                <div className="nav-brand">
-                    <h1 onClick={handleHomeClick} style={{cursor: 'pointer'}}>Next Meal</h1>
-                </div>
-                <div className="nav-other-choices">
-                    <ul>
-                        <li><SearchFoodBtn /></li>
                         <li>Login</li>
                     </ul>
-                    {/* test */}
                 </div>
             </div>
         </div>
