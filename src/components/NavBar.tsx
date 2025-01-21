b90fe679977fd78a3e4e12e17576610ac9a751cb
Marco <marco.a.romero0303@gmail.com>
2025-01-20 23:45:25 -0800

    progress on navigation bar on mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchFoodBtn from './SearchFoodBtn.js';
 import '../styles/navBar.scss';
 import { Link } from 'react-router-dom';
 
 function NavBar(){
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function toggleHamburger(){
+        setMenuOpen((prev) => !prev);
+    }
+
     return(
-        <div className="header-section">
-            <div className="nav-bar">
-                <div className="nav-brand">
-                    <h1><Link to="/">Next Meal</Link></h1>
-                </div>
-                <div className="nav-other-choices">
-                    <SearchFoodBtn />
-                </div>
-                <div className="nav-links">
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li>Food Menu</li>
-                        <li>About</li>
-                        <li>Other</li>
-                        <li>Login</li>
-                    </ul>
-                </div>
+        <header>
+            <div className="header-section">
+                <nav className="nav-bar desktop">
+                    <div className="nav-brand">
+                        <h1><Link to="/">Next Meal</Link></h1>
+                    </div>
+                    <div className="nav-other-choices">
+                        <SearchFoodBtn />
+                    </div>
+                    <div className="nav-links">
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li>Food Menu</li>
+                            <li>About</li>
+                            <li>Other</li>
+                            <li>Login</li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
-        </div>
+
+            <div className="header-section">
+                <nav className="nav-bar mobile">
+                    <button className="hamburger-icon" onClick={toggleHamburger}>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                    </button>
+                    <div className="nav-brand">
+                        <h1><Link to="/">Next Meal</Link></h1>
+                    </div>
+                    <div className="nav-other-choices">
+                        <SearchFoodBtn />
+                    </div>
+                    {menuOpen && (
+                        <ul className="mobile-nav-links">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/">Food Menu</Link></li>
+                            <li><Link to="/">About</Link></li>
+                            <li><Link to="/">Other</Link></li>
+                            <li><Link to="/">Login</Link></li>
+                        </ul>
+                    )}
+                </nav>
+            </div>
+        </header>
     )
 }
 
