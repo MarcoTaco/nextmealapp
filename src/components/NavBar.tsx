4859c472c39fa8ca10ac8c3f151c0ce803282447
Marco <marco.a.romero0303@gmail.com>
2025-02-23 17:58:49 -0800

    updated hamburger menu for mobile. made it so if a user clicks outside the menu, the menu closes. styled it to work correctly. added a close button. more styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import SearchFoodBtn from './SearchFoodBtn.js';
 import '../styles/navBar.scss';
 import { Link } from 'react-router-dom';
 
 function NavBar(){
     const [menuOpen, setMenuOpen] = useState(false);
+    const hamburgerMenuRef = useRef<HTMLDivElement>(null);
 
-    function toggleHamburger(){
-        setMenuOpen((prev) => !prev);
-    }
+    function toggleHamburger() {
+        setMenuOpen(!menuOpen);
+    }  
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if(hamburgerMenuRef.current && !hamburgerMenuRef.current.contains(event.target as Node)) {
+                setMenuOpen(false);
+            }
+        }
+
+        if(menuOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+    }, [menuOpen]);
 
     return(
         <header>
@@ -31,7 +48,7 @@ function NavBar(){
                     </div>
                 </nav>
             </div>
-
+            
             <div className="header-section">
                 <nav className="nav-bar mobile">
                     <button className="hamburger-icon" onClick={toggleHamburger}>
@@ -46,13 +63,20 @@ function NavBar(){
                         <SearchFoodBtn />
                     </div>
                     {menuOpen && (
-                        <ul className="mobile-nav-links">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/">Food Menu</Link></li>
-                            <li><Link to="/">About</Link></li>
-                            <li><Link to="/">Other</Link></li>
-                            <li><Link to="/">Login</Link></li>
-                        </ul>
+                        <div className="hamburger-menu" ref={hamburgerMenuRef}>
+                            <ul className="mobile-nav-links">
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/">Food Menu</Link></li>
+                                <li><Link to="/">About</Link></li>
+                                <li><Link to="/">Other</Link></li>
+                                <li><Link to="/">Login</Link></li>
+                            </ul>
+                            <div className="hamburger-close">
+                                <button className="hamburger-close-icon" onClick={() => setMenuOpen(false)}>
+                                    <span className="x">x</span>
+                                </button>
+                            </div>
+                        </div>
                     )}
                 </nav>
             </div>
