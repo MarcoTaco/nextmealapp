8ec9195e6280a53c5d9130e0313807c8563f4bc1
Marco <marco.a.romero0303@gmail.com>
2025-04-13 23:22:24 -0700

    started working on displaying saved recipes from each user

--- src/pages/UserSavedFoods.tsx
+++ src/pages/UserSavedFoods.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 function UserSavedFoods() {
     return(
-        <>
-        </>
+       <div className="saved-foods-results-section">
+        <div className="saved-foods-header">
+            <h1>Name's Saved Foods</h1>
+        </div>
+        <div className="saved-foods-results">
+            {/* {recipes.map() => (
+                <div className="food-information">
+                    <div className="food-image">
+                        <img />
+                    </div>
+                    <div className="food-name">
+                        <h4></h4>
+                    </div>
+                </div>
+            )} */}
+        </div>
+       </div>
     );
 }
 
