c510df6e74d36ad9b06587ab78ecc99130887a6d
Marco <marco.a.romero0303@gmail.com>
2025-04-13 23:22:38 -0700

    forgot to save

--- src/pages/UserSavedFoods.tsx
+++ src/pages/UserSavedFoods.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { collection, query, where, getDocs, doc } from 'firebase/firestore';
 
 function UserSavedFoods() {
+    const{ user } = useAuth0();
+
+    
+
     return(
        <div className="saved-foods-results-section">
         <div className="saved-foods-header">
